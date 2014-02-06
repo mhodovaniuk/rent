bf04c128a05e04aa68fa0ea5296079ac792d362e
Mykhailo Hodovaniuk <Mykhailo_Hodovaniuk@epam.com>
2014-02-06 16:20:20 +0200

    none

--- src/main/webapp/resources/js/jq.js
+++ src/main/webapp/resources/js/jq.js
@@ -1,6 +1,6 @@
 $(function(){
     $("a").each(function(){
-        if ($(this).attr("href") == window.location.pathname){
+        if ($(this).attr("href") == window.location.pathname && window.location.pathname!="/rent/login.xhtml" && window.location.pathname!="/rent/register.xhtml"){
             $(this).addClass("selected");
         }
     });
